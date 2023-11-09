6d99301cd062942d59a0bab3ff97be7d5dd4f470
maple3142 <kirby741852963@gmail.com>
2023-11-09 21:07:07 +0800

    Update db schema

--- src/db/types.ts
+++ src/db/types.ts
@@ -24,6 +24,13 @@ export type PaymentType = "cash" | "credit_card" | "monthly";
 
 export type PrivilegeType = "consumer" | "store_manager";
 
+export type Timestamp = ColumnType<Date, Date | string, Date | string>;
+
+export interface MealAssoc {
+  meal_id: number;
+  tag_id: number;
+}
+
 export interface Meals {
   category: string;
   customizations: Json | null;
@@ -33,12 +40,7 @@ export interface Meals {
   name: string;
   picture: string | null;
   price: number;
-  store: number;
-}
-
-export interface MealTagAssociation {
-  meal_id: number;
-  tag_id: number;
+  store_id: number;
 }
 
 export interface MealTags {
@@ -46,38 +48,29 @@ export interface MealTags {
   name: string;
 }
 
-export interface Order {
-  created_at: string;
+export interface OrderDetails {
+  id: Generated<number>;
+  meal_id: number;
+  notes: string | null;
+  order_id: number;
+  quantity: number;
+}
+
+export interface Orders {
+  created_at: Generated<Timestamp>;
   delivery_method: string;
   id: Generated<number>;
-  notes: string;
+  notes: string | null;
   payment_type: PaymentType;
   state: OrderState;
-  user: number;
+  user_id: number;
 }
 
-export interface OrderDetail {
-  id: Generated<number>;
-  meal: number;
-  notes: string;
-  number: number;
-}
-
-export interface OrderDetailAssociation {
-  order_detail_id: number;
-  order_id: number;
-}
-
-export interface StoreData {
+export interface Stores {
   description: string | null;
   id: Generated<number>;
   name: string;
   owner_id: number;
-}
-
-export interface StoreTagAssociation {
-  store_id: number;
-  tag_id: number;
 }
 
 export interface StoreTags {
@@ -85,34 +78,38 @@ export interface StoreTags {
   name: string;
 }
 
-export interface User {
+export interface StoreTagsAssoc {
+  store_id: number;
+  tag_id: number;
+}
+
+export interface UserLogin {
+  password: string;
+  user_id: number;
+  username: string;
+}
+
+export interface UserPrivileges {
+  privilege: PrivilegeType;
+  user_id: number;
+}
+
+export interface Users {
   id: Generated<number>;
   login_type: LoginType;
   name: string;
 }
 
-export interface UserData {
-  login_id: Generated<number>;
-  password: string;
-  username: string;
-}
-
-export interface UserPrivilege {
-  privilege: PrivilegeType;
-  user_id: number;
-}
-
 export interface DB {
-  meal_tag_association: MealTagAssociation;
+  meal_assoc: MealAssoc;
   meal_tags: MealTags;
   meals: Meals;
-  order: Order;
-  order_detail: OrderDetail;
-  order_detail_association: OrderDetailAssociation;
-  store_data: StoreData;
-  store_tag_association: StoreTagAssociation;
+  order_details: OrderDetails;
+  orders: Orders;
   store_tags: StoreTags;
-  user: User;
-  user_data: UserData;
-  user_privilege: UserPrivilege;
+  store_tags_assoc: StoreTagsAssoc;
+  stores: Stores;
+  user_login: UserLogin;
+  user_privileges: UserPrivileges;
+  users: Users;
 }
