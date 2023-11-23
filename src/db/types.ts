b74e7391869865aabc09a89eb44bd86d200a3c94
maple3142 <kirby741852963@gmail.com>
2023-11-23 14:39:55 +0800

    Format db/types.ts

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,115 +1,115 @@
-import type { ColumnType } from "kysely";
+import type { ColumnType } from 'kysely'
 
 export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
-  ? ColumnType<S, I | undefined, U>
-  : ColumnType<T, T | undefined, T>;
+	? ColumnType<S, I | undefined, U>
+	: ColumnType<T, T | undefined, T>
 
-export type Json = ColumnType<JsonValue, string, string>;
+export type Json = ColumnType<JsonValue, string, string>
 
-export type JsonArray = JsonValue[];
+export type JsonArray = JsonValue[]
 
 export type JsonObject = {
-  [K in string]?: JsonValue;
-};
+	[K in string]?: JsonValue
+}
 
-export type JsonPrimitive = boolean | number | string | null;
+export type JsonPrimitive = boolean | number | string | null
 
-export type JsonValue = JsonArray | JsonObject | JsonPrimitive;
+export type JsonValue = JsonArray | JsonObject | JsonPrimitive
 
-export type LoginType = "regular" | "sso";
+export type LoginType = 'regular' | 'sso'
 
-export type OrderState = "aborted" | "paid" | "unpaid";
+export type OrderState = 'aborted' | 'paid' | 'unpaid'
 
-export type PaymentType = "cash" | "credit_card" | "monthly";
+export type PaymentType = 'cash' | 'credit_card' | 'monthly'
 
-export type PrivilegeType = "consumer" | "store_manager";
+export type PrivilegeType = 'consumer' | 'store_manager'
 
-export type Timestamp = ColumnType<Date, Date | string, Date | string>;
+export type Timestamp = ColumnType<Date, Date | string, Date | string>
 
 export interface MealAssoc {
-  meal_id: number;
-  tag_id: number;
+	meal_id: number
+	tag_id: number
 }
 
 export interface Meals {
-  category: string;
-  customizations: Json | null;
-  description: string | null;
-  id: Generated<number>;
-  is_available: Generated<boolean>;
-  name: string;
-  picture: string | null;
-  price: number;
-  store_id: number;
+	category: string
+	customizations: Json | null
+	description: string | null
+	id: Generated<number>
+	is_available: Generated<boolean>
+	name: string
+	picture: string | null
+	price: number
+	store_id: number
 }
 
 export interface MealTags {
-  id: Generated<number>;
-  name: string;
+	id: Generated<number>
+	name: string
 }
 
 export interface OrderDetails {
-  id: Generated<number>;
-  meal_id: number;
-  notes: string | null;
-  order_id: number;
-  quantity: number;
+	id: Generated<number>
+	meal_id: number
+	notes: string | null
+	order_id: number
+	quantity: number
 }
 
 export interface Orders {
-  created_at: Generated<Timestamp>;
-  delivery_method: string;
-  id: Generated<number>;
-  notes: string | null;
-  payment_type: PaymentType;
-  state: OrderState;
-  user_id: number;
+	created_at: Generated<Timestamp>
+	delivery_method: string
+	id: Generated<number>
+	notes: string | null
+	payment_type: PaymentType
+	state: OrderState
+	user_id: number
 }
 
 export interface Stores {
-  description: string | null;
-  id: Generated<number>;
-  name: string;
-  owner_id: number;
+	description: string | null
+	id: Generated<number>
+	name: string
+	owner_id: number
 }
 
 export interface StoreTags {
-  id: Generated<number>;
-  name: string;
+	id: Generated<number>
+	name: string
 }
 
 export interface StoreTagsAssoc {
-  store_id: number;
-  tag_id: number;
+	store_id: number
+	tag_id: number
 }
 
 export interface UserLogin {
-  password: string;
-  user_id: number;
-  username: string;
+	password: string
+	user_id: number
+	username: string
 }
 
 export interface UserPrivileges {
-  privilege: PrivilegeType;
-  user_id: number;
+	privilege: PrivilegeType
+	user_id: number
 }
 
 export interface Users {
-  id: Generated<number>;
-  login_type: LoginType;
-  name: string;
+	id: Generated<number>
+	login_type: LoginType
+	name: string
 }
 
 export interface DB {
-  meal_assoc: MealAssoc;
-  meal_tags: MealTags;
-  meals: Meals;
-  order_details: OrderDetails;
-  orders: Orders;
-  store_tags: StoreTags;
-  store_tags_assoc: StoreTagsAssoc;
-  stores: Stores;
-  user_login: UserLogin;
-  user_privileges: UserPrivileges;
-  users: Users;
+	meal_assoc: MealAssoc
+	meal_tags: MealTags
+	meals: Meals
+	order_details: OrderDetails
+	orders: Orders
+	store_tags: StoreTags
+	store_tags_assoc: StoreTagsAssoc
+	stores: Stores
+	user_login: UserLogin
+	user_privileges: UserPrivileges
+	users: Users
 }
